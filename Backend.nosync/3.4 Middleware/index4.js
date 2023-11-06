877c1596398c1508e74bbede95fecc487eb54c8d
ybakyurek <ybakyurek@gmail.com>
2023-11-06 15:39:48 +0300

    Example of Middlewares

---
+++ Backend.nosync/3.4 Middleware/index4.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+// app.use(morgan("combined")); // Log requests to the console
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function bandNamelogger(req, res, next) {
+  console.log("Request Method: ", req.method);
+  console.log("Request URL: ", req.url);
+  console.log("Request Object: ", req.body);
+  console.log("Object attribute: " + req.body["street"] + req.body["pet"]);
+  next();
+}
+
+app.use(bandNamelogger);
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+  });
+
+  app.post("/submit", (req, res) => {
+    res.send("<h1>Your Hero Name</h1><p>" + req.body.street + req.body.pet +"</p>");
+    console.log("Street: " + req.body.street + "\n" + "Pet: "  + req.body.pet);
+  });
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+
+
+
