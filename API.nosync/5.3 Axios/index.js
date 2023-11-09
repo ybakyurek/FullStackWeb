e753db44d05cd0dc23747d2bdc53946a62f2b4e0
ybakyurek <ybakyurek@gmail.com>
2023-11-09 14:22:18 +0300

    random activity

--- API.nosync/5.3 Axios/index.js
+++ API.nosync/5.3 Axios/index.js
@@ -25,17 +25,33 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
 
-  // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
-  // Render the index.ejs file with a single *random* activity that comes back
-  // from the API request.
-  // Step 3: If you get a 404 error (resource not found) from the API request.
-  // Pass an error to the index.ejs to tell the user:
-  // "No activities that match your criteria."
+  try {
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`
+    );
+    const result = response.data;
+    const randomIndex = Math.floor(Math.random() * result.length)
+    res.render("index.ejs", { data: result[randomIndex] });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render(
+      "index.ejs",
+      {
+        error: "No activities that match your criteria."
+      }
+    );
+  }
 });
+
+// Step 2: Play around with the drop downs and see what gets logged.
+// Use axios to make an API request to the /filter endpoint. Making
+// sure you're passing both the type and participants queries.
+// Render the index.ejs file with a single *random* activity that comes back
+// from the API request.
+// Step 3: If you get a 404 error (resource not found) from the API request.
+// Pass an error to the index.ejs to tell the user:
+// "No activities that match your criteria."
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
